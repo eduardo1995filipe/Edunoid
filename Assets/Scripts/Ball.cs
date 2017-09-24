39f2eb8cd670d933c8b6d08540e2d78646d90100
Eduardo Bagarrao <eduardo_95@yahoo.com>
2017-09-24 11:26:10 +0100

    Project Startup
    - Added first  level soundtrack;
    - Added walls, background, racket and ball;
    - Added some physics and colliders

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		this.speed = 100.0f;
+		this.rb = GetComponent<Rigidbody2D> ();
+
+		rb.velocity = Vector2.up * speed;
+	}
+
+
+	float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth){
+		return (ballPos.x - racketPos.x)/racketWidth;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+    // Hit the Racket?
+    if (col.gameObject.name == "racket") {
+        // Calculate hit Factor
+        float x=hitFactor(transform.position,
+                          col.transform.position,
+                          col.collider.bounds.size.x);
+
+        // Calculate direction, set length to 1
+        Vector2 dir = new Vector2(x, 1).normalized;
+
+        // Set Velocity with dir * speed
+			GetComponent<Rigidbody2D> ().velocity = dir * speed;
+    }
+}
+
+}
---
+++ Assets/Scripts/RacketMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketMovement : MonoBehaviour {
+
+	//Movement speed
+	public float speed = 150;
+
+	void Start(){
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+		rb.freezeRotation = true;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Get Horizontal Input
+		float h = Input.GetAxisRaw("Horizontal");
+
+		// Set Velocity (movement direction * speed)
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+	}
+}
